8bfa13d73791015066e6fed3656309ecf7c8016c
zavx0z <metaversebdfl@gmail.com>
2024-04-03 19:06:42 +0300

    получение ссылки на элемент <canvas>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("модуль index.js загружен")
+/** Получение ссылки на элемент Canvas
+ * @param {Object} [options={}] - Объект с параметрами
+ * @param {boolean} [options.debug=false] - Выводить отладочную информацию
+ * @returns {HTMLCanvasElement} - Элемент холста
+ */
+export const getCanvas = ({ debug = false } = {}) => {
+  const canvas = document.querySelector("canvas")
+  if (!canvas) throw new Error("Не найден элемент canvas.")
+  if (debug) {
+    const canvasWidth = canvas.width || canvas.clientWidth
+    const canvasHeight = canvas.height || canvas.clientHeight
+    console.log("======================= CANVAS =======================")
+    console.log(`Найден элемент canvas с размерами ${canvasWidth}x${canvasHeight}`)
+  }
+  return canvas
+}
